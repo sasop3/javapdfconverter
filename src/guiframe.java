7240fb28439aa5fdb14dc05f64973a6173ab48d6
SAS_OP <14040386+sasop3@users.noreply.github.com>
2025-04-07 18:24:09 +0300

    Edited the Jframe file and added the defaults

--- src/guiframe.java
+++ src/guiframe.java
@@ -1,6 +1,15 @@
 import javax.swing.JFrame;
 
 public class guiframe extends JFrame {
-    
-    
+  
+    guiframe()
+    {
+        setTitle("Program");
+        setSize(400, 400);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        
+    }
 }
