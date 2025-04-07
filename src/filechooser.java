fe348ee7a05e536a0791a082cb2d0afcabc400f7
SAS_OP <14040386+sasop3@users.noreply.github.com>
2025-04-07 20:16:49 +0300

    added uploading and a textlabel that takes the value of the file selected in the uploading button

---
+++ src/filechooser.java
@@ -0,0 +1,26 @@
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import java.io.File;
+public class filechooser extends JFrame {
+
+
+    private final JFileChooser fileCH;
+
+    filechooser()
+    {
+        
+        fileCH = new JFileChooser();
+        fileCH.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        int option = fileCH.showOpenDialog(null);
+        if(option == fileCH.APPROVE_OPTION)
+        {
+            File Selectedfile = fileCH.getSelectedFile();
+            guiframe.label.setText(Selectedfile.getName());
+        }
+        add(fileCH);
+        
+    }
+}
--- src/guiframe.java
+++ src/guiframe.java
@@ -1,15 +1,50 @@
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 public class guiframe extends JFrame {
-  
+    
+    protected File file;
+    private final JButton button;
+    private final JButton button2;
+    protected static JLabel label;
     guiframe()
     {
         setTitle("Program");
-        setSize(400, 400);
+        setSize(400, 200);
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+       
+        label = new JLabel("null");
+        label.setForeground(Color.red);
         
+        button = new JButton("Choose File");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new filechooser();
+            }
+        });
         
+        button2 = new JButton("Convert");
+
+
+        add(label);
+        add(button,new BorderLayout().CENTER);
+        add(button2,new BorderLayout().SOUTH);
+       
+        
+
+
+        revalidate();
     }
 }
