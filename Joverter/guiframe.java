46368fc1f6632d104ce5625186e77164a2068f5c
SAS_OP <14040386+sasop3@users.noreply.github.com>
2025-04-08 18:14:48 +0300

    save for next commitment

--- Joverter/guiframe.java
+++ Joverter/guiframe.java
@@ -1,8 +1,6 @@
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
