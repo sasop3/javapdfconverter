030af48c14adcc7ead78e713abaaa97770ccbf0c
sasop <sasop@hotmail.com>
2025-06-01 14:03:09 +0300

    Now images converted from the program will be stored in a folder

--- src/guiframe.java
+++ src/guiframe.java
@@ -4,10 +4,9 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
@@ -15,50 +14,44 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import org.apache.pdfbox.*;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.apache.pdfbox.rendering.PDFRenderer;
 
-
 public class guiframe extends JFrame {
-    
+
     protected File file;
     private JFileChooser fileCS;
     private final JButton button;
     private final JButton button2;
     protected static JLabel label;
-    guiframe()
-    {
+
+    guiframe() {
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }catch(UnsupportedLookAndFeelException e)
-        {
+        } catch (UnsupportedLookAndFeelException e) {
             try {
-                
+
                 UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
             } catch (Exception r) {
                 System.err.println("Error code 123");
             }
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-
 
         setTitle("Program");
         setSize(400, 200);
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
-       
+        setLayout(new FlowLayout(FlowLayout.CENTER, 30, 30));
+
         label = new JLabel("null");
         label.setForeground(Color.red);
-        
+
         button = new JButton("Choose File");
         button.addActionListener(new ActionListener() {
             @Override
@@ -66,49 +59,49 @@ public class guiframe extends JFrame {
                 fileCS = new JFileChooser();
                 fileCS.setFileSelectionMode(JFileChooser.FILES_ONLY);
                 int option = fileCS.showOpenDialog(null);
-                if(option == fileCS.APPROVE_OPTION)
-                {
+                if (option == JFileChooser.APPROVE_OPTION) {
                     file = fileCS.getSelectedFile();
                     label.setText(file.getName());
                 }
 
             }
         });
-        
+
         button2 = new JButton("Convert");
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+
                 try {
                     PDDocument doc = PDDocument.load(file);
                     PDFRenderer Prender = new PDFRenderer(doc);
                     for (int i = 0; i < doc.getNumberOfPages(); i++) {
                         BufferedImage image;
-                        image = Prender.renderImage(i); // need to get all the indexes from the pdf pending
-                        ImageIO.write(image, "JPEG",new File(String.format("PageNumber_%d.jpg", i)));                    
+                        image = Prender.renderImage(i);
+                        File imgFile = new File("Image Folder"); // This makes Output Folder
+                        if (!imgFile.exists()) {
+                            boolean created = imgFile.mkdirs();
+                            if (!created) {
+                                JOptionPane.showMessageDialog(null, "FOLDER ERROR");
+                                System.exit(1);
+                            }
+                        }
+
+                        ImageIO.write(image, "JPEG", new File(imgFile, String.format("PageNumber_%d.jpg", i)));
                     }
+
                     doc.close();
-                } 
-                catch (InvalidPasswordException exception) {
-                    exception.printStackTrace();
+                } catch (InvalidPasswordException exception) {
+                    JOptionPane.showMessageDialog(null, "The pdf is protected by a password", "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                } catch (IOException exception) {
+                    JOptionPane.showMessageDialog(null, "An IO Error has occured", "Error", JOptionPane.ERROR_MESSAGE);
                 }
-                catch(IOException exception) {
-                    exception.printStackTrace();
-                }
-
-
-
             }
         });
 
-
-        add(label);
         add(button);
         add(button2);
-       
-        
-
 
         revalidate();
     }
